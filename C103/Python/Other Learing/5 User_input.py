2ee14c5a9235819f61a4542099ae65e5f2c2286e
HyperDusXDeveloper <HyperDusXDeveloper@gmail.com>
2024-09-11 01:06:48 +0700

    del

--- C103/Python/Other Learing/5 User_input.py
+++
@@ -1,8 +0,0 @@
-name = input("What is You Name ? : ")
-age = int(input("How old are you ? :"))
-height = float(input("How tall are you ? : "))
-age = age+1
-print("Hello"+name)
-print("you are ", age ,"year old")
-#print("You are "+ str(age)+"year old")
-print("you are " + str(height)+ "cm Tall")
