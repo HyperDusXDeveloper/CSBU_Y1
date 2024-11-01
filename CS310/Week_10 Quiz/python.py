7e759366d464497257239e80afdc860e011ab0bc
HyperDusXDeveloper <HyperDusXDeveloper@gmail.com>
2024-11-02 05:32:25 +0700

    Python text

--- CS310/Week_10 Quiz/python.py
+++ CS310/Week_10 Quiz/python.py
@@ -1,3 +1,6 @@
-with open("data/Score.txt" , "r") as file :
+with open("Week_10 Quiz/data/Score.txt" , "r") as file :
     data_Score = file.readlines()
     print(data_Score)
+    for line in data_Score :
+        word = line.split()
+        print(line)
