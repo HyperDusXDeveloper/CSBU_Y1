398328ecca068e4a4449eae0dbf2f424d4a21f7b
HyperDusXDeveloper <HyperDusXDeveloper@gmail.com>
2024-11-02 02:04:21 +0700

    upload

---
+++ CS310/Week_10 Quiz/python.py
@@ -0,0 +1,3 @@
+with open("data/Score.txt" , "r") as file :
+    data_Score = file.readlines()
+    print(data_Score)
--- CS310/Week_9/Act1_1670700044.py
+++ CS310/Week_9/Act1_1670700044.py
@@ -1,8 +1,8 @@
 print("-"*45)
-with open("data/information.txt","r") as file :
+with open("Week_9/data/information.txt","r") as file :
     data = file.readlines()
-    # print(data)
-    for line in data :
-        word = line.split()
-        print(word[0],word[3],word[1],word[2])
-print("-"*45)
+    print(data)
+#     for line in data :
+#         word = line.split()
+#         print(word[0],word[3],word[1],word[2])
+# print("-"*45)
