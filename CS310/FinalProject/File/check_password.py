1e86c85a536bec41f22288bfb1c33dc693b48f00
HyperDusXDeveloper <HyperDusXDeveloper@gmail.com>
2024-11-21 05:39:43 +0700

    update

--- CS310/FinalProject/File/check_password.py
+++ CS310/FinalProject/File/check_password.py
@@ -9,3 +9,18 @@ while not check_password_found:
             break  
     if not check_password_found:
         print("Invalid password, please try again.")
+
+# def regester():
+#     title(3)
+#     username = input("Please Enter Username : ")
+#     password = input("Please Enter Password : ")
+#     check_password_found = False
+#     while not check_password_found:
+#         confirm_password = input("Please Confirm Password : ")
+#         for check in password:
+#             if confirm_password == password: 
+#                 print("Regester Succeed !")
+#                 check_password_found = True 
+#                 break
+#         if not check_password_found:
+#             print("Invalid Confirm Password Please Try Again.")
--- CS310/FinalProject/Final Login.py
+++ CS310/FinalProject/Final Login.py
@@ -1,4 +1,4 @@
-# Random Word 
+# Random Word
 def title(title_num):
     if title_num == 1 : # Start Game Word Guessing Game MENU
        print("-"*40)
@@ -16,49 +16,74 @@ def title(title_num):
        print("-"*40)
        print(f"LOGIN".center(40))
        print("-"*40)
-    elif title_num == 5 : # Game Menu 
+    elif title_num == 5 : # Game Menu
         print(f"- SELECT MENU -".center(40))
         print("-"*40)
         print(f"1 Play\n2 Profile\n3 Logout")
         print("-"*40)
 
+# def main() :
+#     print("Login Succeed !")
+
 def regester():
     title(3)
-    username = input("Please Enter Username : ")
-    password = input("Please Enter Password : ")
-    check_password_found = False
-    while not check_password_found:
-        confirm_password = input("Please Confirm Password : ")
-        for check in password:
-            if confirm_password == password: 
-                print("Regester Succeed !")
-                check_password_found = True
-                break  
-        if not check_password_found:
-            print("Invalid Password Please Try Again.")
+    check_username_found = True
+    while check_username_found :
+        with open("data_user.txt","r") as file :
+            username = input("Please Enter Username : ")
+            data = file.readlines()
+            check_username = False
+            for i in data :
+                item = i.split()
+                if username == item[0] :
+                    check_username = True
 
+            if check_username :
+                print("This Username Already Exists!")
+            else :
+                check_username_found = False
+                
+    password = input("Please Enter Password : ")
+
+    check_password_found = True
+    while check_password_found :
+        confirm_password = input("Please Confirm Password : ")
+        if password != confirm_password :
+            print("Invalid Confirm Password Please Try Again.")
+        else :
+            print("Regester Succeed !")
+            check_password_found = False
+            
+    with open("data_user.txt","a") as file :
+        file.write(f"{username} {password}\n")
 
 def login() :
     title(4)
     username = input("Please Enter Username : ")
     password = input("Please Enter Password : ")
+    with open("data_user.txt","r") as file :
+        data = file.readlines()
+        check_fail = True
+        for i in data :
+            item = i.split()
+            if username == item[0] and password == item[1] :
+                print("Login Succeed !")
+                check_fail = False
+                
+        if check_fail :
+            print("Invalid Try Again.")
 
-
-
-def login_main():
-    login_run = True
-    while login_run :
-        title(1)
-        title(2)
-        user_input = int(input("Please Choich Number : "))
-        if user_input == 1 :
-            regester()
-        elif user_input == 2 :
-            login()
-        elif user_input == 3 :
-            login_run = False
-        else :
-            print("Incorrect Menu !! Pleasse Try Again ")
-        
-login_main()
-
+login_run = True
+while login_run :
+    title(1)
+    title(2)
+    user_input = int(input("Please Choich Number : "))
+    if user_input == 1 :
+        regester()
+    elif user_input == 2 :
+        login()
+    elif user_input == 3 :
+        login_run = False
+    else :
+        print("Incorrect Menu !! Pleasse Try Again ")
+    
