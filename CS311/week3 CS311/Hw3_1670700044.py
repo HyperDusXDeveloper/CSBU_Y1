b7a1544129db8d80738e3010a5317fd7232c46f1
HyperDusXDeveloper <HyperDusXDeveloper@gmail.com>
2025-02-07 03:08:20 +0700

    Upload

--- CS311/week3 CS311/Hw3_1670700044.py
+++ CS311/week3 CS311/Hw3_1670700044.py
@@ -1,29 +1,37 @@
 from tkinter import *
 
-def createwindow() : #สร้างหน้าต่าง
+def createwindow() :
     master = Tk()
-    master.wm_geometry('1200x900')
+    master.wm_geometry('1200x700')
+
     master.grid_rowconfigure((0,2),weight=1)
-    master.grid_rowconfigure(1,weight=4)
+    master.grid_rowconfigure(1,weight=3)
     master.grid_columnconfigure(0,weight=2)
     master.grid_columnconfigure(1,weight=1)
-    master.option_add('*font','Garamond 20')
+
+    master.title('Homework Week 3 : Design Layout Using Frame')
+    master.option_add('*font','Arial 20')
     return(master)
 
-def layout(master): #
+def layout(master): 
     top = Frame(master,bg='#D9EAFD')
     top.grid(row=0,columnspan=2,sticky='news')
+    top.grid_rowconfigure((0,1),weight=1)
+    top.grid_columnconfigure((0,1),weight=1)
 
 
-    left = Frame(master,bg='#FFA09B')
+    left = Frame(master,bg='#D1F4F4')
     left.grid(row=1,column=0,sticky='news',padx=10 , pady=15)
-    left.grid_rowconfigure((0,1),weight=1)
-    left.grid_columnconfigure((0,1),weight=1)
+    left.grid_rowconfigure(0,weight=2)
+    left.grid_rowconfigure(1,weight=3)
+    left.grid_rowconfigure(2,weight=1)
+    left.grid_columnconfigure((0),weight=1)
+    left.grid_columnconfigure((1,2),weight=3)
 
     right = Frame(master,bg='#EFB036')  
     right.grid(row=1,column=1,sticky='news',padx=10 , pady=15) 
     right.grid_rowconfigure((0,1),weight=1)
-    right.grid_columnconfigure((0,1),weight=1)
+    right.grid_columnconfigure((0),weight=1)
 
     bottom = Frame(master,bg='#26355D')
     bottom.grid(row=2,columnspan=2,sticky='news')
@@ -33,39 +41,40 @@ def layout(master): #
     return(top,left,right,bottom)
 
 def widgets(top,left,right,bottom) :
+
     #TOP
-    heading = Label(top,text="Dashboard DIY By Natchanon Saileamonpiwat ",fg='Black',font=('comic sans ms',25,'bold'),bg="#D9EAFD")
-    heading.pack(pady=15)
+    heading = Label(top,text="Dashboard DIY By Natchanon Saileamonpiwat ",fg='Black',font=('Arial',25,'bold'),bg="#D9EAFD",width=50)
+    heading.grid(columnspan=2,rowspan=2)
 
     #LEFT
-    widgetsLeftIMG1 = Label(left,image=Profile,bg='white')
+    widgetsLeftIMG1 = Label(left,image=Profile,bg='#D1F4F4',fg='Black')
     widgetsLeftIMG1.grid(row=0,column=0)
-    textleft = Label(left,text="Natchanon Saileamonpiwat ",fg='Black',font=('comic sans ms',25,'bold'),bg="#D9EAFD")
-    textleft = Label(left,text="Natchanon Saileamonpiwat ",fg='Black',font=('comic sans ms',25,'bold'),bg="#D9EAFD")
-    textleft.grid(row=0,column=1)
+
+    leftname = Label(left,text="Name : Natchanon Saileamonpiwat \nCollegian \nInformation Technology and Innovation",fg='Black',font=('Arial',15,'bold'),bg="#D1F4F4",justify='left')#.place(x=350,y=120)
+    leftname.grid(row=0,column=1)
+
     widgetsLeftIMG2 = Label(left,image=barcolor,bg='white')
-    widgetsLeftIMG2.grid(row=1,columnspan=2)
+    widgetsLeftIMG2.grid(rowspan=3,columnspan=2)
 
     #RIGHT
     widgetsRightIMG1 = Label(right,image=skill,bg='white')
-    widgetsRightIMG1.grid(row=0,columnspan=3)
+    widgetsRightIMG1.grid(row=0,column=0,columnspan=2,rowspan=2)
 
-    #Botton
-    # bottom.option_add('*font','Garamond 20')
-    btn1 = Button(bottom,text="Click Me 1",font=('comic sans ms',15,'bold'))
-    btn1.grid(row=0,column=0)
+    #BOTTOM
+    btn1 = Button(bottom,text="Click Me 1",font=('Arial',15,'bold'))
+    btn1.grid(row=0,column=0,ipadx=50,ipady=15,padx=5,pady=5)
     
-    btn2 = Button(bottom,text="Click Me 2",width=15,font=('comic sans ms',15,'bold'))
-    btn2.grid(row=0,column=1)
+    btn2 = Button(bottom,text="Click Me 2",width=15,font=('Arial',15,'bold'))
+    btn2.grid(row=0,column=1,ipadx=10,ipady=15,padx=5,pady=5)
 
-    btn3 = Button(bottom,text="Click Me 3",width=15,font=('comic sans ms',15,'bold'))
-    btn3.grid(row=0,column=2)
+    btn3 = Button(bottom,text="Click Me 3",width=15,font=('Arial',15,'bold'))
+    btn3.grid(row=0,column=2,ipadx=15,ipady=15,padx=5,pady=5)
     
-    btn4 = Button(bottom,text="Exit Program",width=15,font=('comic sans ms',15,'bold'),command=exit)
-    btn4.grid(row=0,column=3)
+    btn4 = Button(bottom,text="Exit Program",width=15,font=('Arial',15,'bold'),command=exit)
+    btn4.grid(row=0,column=3,ipadx=15,ipady=15,padx=5,pady=5)
 
 master = createwindow()
-Profile = PhotoImage(file='week3 CS311/image/female.png').subsample(2,2)
+Profile = PhotoImage(file='week3 CS311/image/AT.png').subsample(2,2)
 barcolor = PhotoImage(file='week3 CS311/image/barcolor.png')
 skill = PhotoImage(file='week3 CS311/image/skill.png')
 top,left,right,bottom = layout(master)
