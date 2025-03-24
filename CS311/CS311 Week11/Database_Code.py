4245a9e32b2e90579f54cf5184baf3b2bca42bf9
Atommic <HyperDusXDeveloper@gmail.com>
2025-03-24 16:14:09 +0700

    Update Week11

---
+++ CS311/CS311 Week11/Database_Code.py
@@ -0,0 +1,14 @@
+print( " 1 เปลี่ยนข้อมูลใน Database ") 
+# UPDATE login
+# set user = 'pond1' , name='Natchanon Saileamonpiwat' , pwd='pwd000'
+# where user = 'ning8'
+
+
+
+
+
+
+#----------------------------------------------------------------
+#ข้อควรระวัง 
+# UPDATE login
+# set user = 'Carefull' เปลี่ยนข้อมูล user ทั้งหมดเป็น 'Carefull'
